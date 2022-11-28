a709608a5ab801107c6d59ec4059a8a52bec740f
liaofubing <2303212350@qq.com>
2022-11-28 15:30:31 +0800

    解决冲突

--- babel.config.js
+++ babel.config.js
@@ -3,18 +3,14 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ],
   "plugins":[
-<<<<<<< HEAD
       ["component",
-=======
-      "component",
->>>>>>> fda710d5cbe8dd1499169c2372616eaa79a948f4
+
     {
       "libraryName":"element-ui",
       "styleLibraryName": "theme-chalk"
     }
-<<<<<<< HEAD
-  ]]
-=======
+
+    ]
   ]
->>>>>>> fda710d5cbe8dd1499169c2372616eaa79a948f4
+
 }
--- src/components/login.vue
+++ src/components/login.vue
@@ -12,55 +12,34 @@
            <input type="password" placeholder="Password" v-model="password">
          </div>
        </div>
-<<<<<<< HEAD
-       <button @click="userLogin">login</button>
-=======
        <button @click="login">login</button>
->>>>>>> fda710d5cbe8dd1499169c2372616eaa79a948f4
      </div>
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
+
 
 import {login} from '../api/login'
 export default {
   data(){
     return{
       username:'aa',
-=======
-export default {
-  data(){
-    return{
-      username:'测试用户',
->>>>>>> fda710d5cbe8dd1499169c2372616eaa79a948f4
       password:'123456'
     }
   },
   methods:{
-<<<<<<< HEAD
-    async userLogin(){
-=======
     async login(){
->>>>>>> fda710d5cbe8dd1499169c2372616eaa79a948f4
       if(this.username.length===0 || this.password.length===0){
         alert('用户名或者密码不能为空')
         return
       }else{
-<<<<<<< HEAD
-        console.log(this.username,this.password)
-        let res = await login(this.username,this.password)
-        if(res.data.code !== 20000) return this.$message.error('登录失败')
-        this.$message.success('登录成功')
-=======
         let jsons = {
           userName: this.username,
           passWord: this.password
         }
         const res = await this.$http.post('login',jsons)
         console.log(res)
->>>>>>> fda710d5cbe8dd1499169c2372616eaa79a948f4
       }
     }
   }
